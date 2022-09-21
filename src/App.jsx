fef2b281168241362ab876d1828fb79fad0f7b76
Jack Long <jack.richard.long@gmail.com>
2022-09-21 16:54:12 -0500

    red cup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import * as THREE from 'three';
-import { useRef, useState } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import * as THREE from "three";
+import { Suspense, useRef, useState } from "react";
+import { Canvas, useFrame, useThree, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-function Box({z}) {
+
+function Box({ z }) {
   const ref = useRef();
-  const { viewport } = useThree()
+  const { viewport, camera } = useThree();
+  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
 
   const [data] = useState({
     x: THREE.MathUtils.randFloatSpread(2),
-    y: THREE.MathUtils.randFloatSpread(viewport.height),
+    y: THREE.MathUtils.randFloatSpread(height),
   });
 
   // subscribe a component to the render-loop
   useFrame((state) => {
-    ref.current.position.set(viewport.width * data.x, (data.y += 0.1), z);
-    if (data.y > viewport.height / 1.5) {
-      data.y = -viewport.height / 1.5;
+    ref.current.position.set(width * data.x, (data.y += 0.5), z);
+    if (data.y > height / 1.5) {
+      data.y = -height / 1.5;
     }
   });
 
@@ -24,13 +27,22 @@ function Box({z}) {
       <boxGeometry />
       <meshBasicMaterial color="orange" />
     </mesh>
-  )
+  );
 }
 
-export default function App({count = 10}) {
+function Apple(props) {
+  const { scene } = useLoader(GLTFLoader, "/red_cup.glb");
+  return <primitive object={scene} {...props} />;
+}
+
+export default function App({ count = 50 }) {
   return (
     <Canvas>
-      {Array.from({ length: count }, (_, i) => (<Box key={i} z={-i} />))}
+      <ambientLight intensity={0.5} />
+      <Suspense fallback={null}>
+        <Apple scale={10}/>
+      </Suspense>
+      {/* {Array.from({ length: count }, (_, i) => (<Box key={i} z={-i} />))} */}
     </Canvas>
   );
 }
