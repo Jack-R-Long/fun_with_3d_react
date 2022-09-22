02a3363c48f9c2edbfd7e64774fdd023dafe3d55
Jack Long <jack.richard.long@gmail.com>
2022-09-22 09:59:09 -0500

    spinning cups baby

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,58 @@
 import * as THREE from "three";
 import { Suspense, useRef, useState } from "react";
 import { Canvas, useFrame, useThree, useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
+import { Environment, useGLTF } from "@react-three/drei";
+import {EffectComposer, DepthOfField} from '@react-three/postprocessing'
 
 function Box({ z }) {
   const ref = useRef();
+
+  const { nodes, materials } = useGLTF("/red_cup-transformed.glb");
+
   const { viewport, camera } = useThree();
   const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
 
   const [data] = useState({
     x: THREE.MathUtils.randFloatSpread(2),
     y: THREE.MathUtils.randFloatSpread(height),
+    rX: Math.random() * Math.PI,
+    rY: Math.random() * Math.PI,
+    rZ: Math.random() * Math.PI,
   });
 
   // subscribe a component to the render-loop
   useFrame((state) => {
-    ref.current.position.set(width * data.x, (data.y += 0.5), z);
+    ref.current.rotation.set(data.rX += 0.001, data.rY += 0.004, data.rZ += 0.005);
+    ref.current.position.set(width * data.x, (data.y += 0.01), z);
     if (data.y > height / 1.5) {
       data.y = -height / 1.5;
     }
   });
 
   return (
-    <mesh ref={ref}>
-      <boxGeometry />
-      <meshBasicMaterial color="orange" />
-    </mesh>
+    <mesh
+      ref={ref}
+      rotation={[-Math.PI / 2, 0, 0]}
+      geometry={nodes.RedCup_RedCup_0.geometry}
+      material={materials.RedCup}
+    />
   );
-}
-
-function Apple(props) {
-  const { scene } = useLoader(GLTFLoader, "/red_cup.glb");
-  return <primitive object={scene} {...props} />;
 }
 
 export default function App({ count = 50 }) {
   return (
     <Canvas>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.2} />
+      <pointLight position={[10, 10, 10]} intensity={2} />
       <Suspense fallback={null}>
-        <Apple scale={10}/>
+        <Environment preset="sunset" />
+        {Array.from({ length: count }, (_, i) => (
+          <Box key={i} z={-i} />
+        ))}
+        <EffectComposer>
+          <DepthOfField target={[0, 0, 30]} focalLength={0.05} bokehScale={11} height={700} />
+        </EffectComposer>
       </Suspense>
-      {/* {Array.from({ length: count }, (_, i) => (<Box key={i} z={-i} />))} */}
     </Canvas>
   );
 }
