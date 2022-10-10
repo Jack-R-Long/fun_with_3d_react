9d8d839c9fe6e9395286c5aa7e6909be897f6aa1
Jack Long <jack.richard.long@gmail.com>
2022-10-10 16:38:38 -0500

    spinning logos concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Physics, usePlane, useBox, Debug } from "@react-three/cannon";
-import { useGLTF, OrbitControls, ContactShadows } from "@react-three/drei";
+import {
+  useGLTF,
+  OrbitControls,
+  ContactShadows,
+  Html,
+} from "@react-three/drei";
 
 function Plane(props) {
   const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
@@ -33,21 +38,75 @@ function Space(props) {
   return <primitive object={scene} {...props} />;
 }
 
+function Dodecahedron({ time, ...props }) {
+  return (
+    <mesh {...props}>
+      <dodecahedronGeometry />
+      <meshStandardMaterial roughness={0.75} emissive="#404057" />
+      <Html distanceFactor={10}>
+        <div className="content">
+          hello <br />
+          world
+        </div>
+      </Html>
+    </mesh>
+  );
+}
+
+function TwitterLogo({ time, ...props }) {
+    const { scene } = useGLTF("/twitter_logo.glb");
+      // const ref = useRef();
+      // useFrame(
+      //   () =>
+      //     (ref.current.rotation.x =
+      //       ref.current.rotation.y =
+      //       ref.current.rotation.z +=
+      //         0.02)
+      // );
+      return (
+        <mesh {...props}>
+          <primitive object={scene} />;
+          <Html distanceFactor={10} position={[2, 0, 0]}>
+            <div className="content">
+              @JackAttack
+            </div>
+          </Html>
+        </mesh>
+      );
+}
+
+
+function Squad() {
+  const ref = useRef();
+  useFrame(
+    () =>
+      (ref.current.rotation.x =
+        ref.current.rotation.y =
+        ref.current.rotation.z +=
+          0.01)
+  );
+  return (
+    <group ref={ref}>
+      <Dodecahedron position={[-2, 0, 0]} />
+      <Dodecahedron position={[0, -2, -3]} />
+      <TwitterLogo position={[2, 0, 0]} />
+    </group>
+  );
+}
+
 export default function App() {
   return (
-    <Canvas camera={{ fov: 50 }}>
+    <Canvas camera={{ position: [0, 0, 7.5] }}>
       <hemisphereLight color="white" groundColor="blue" intensity={0.75} />
       <spotLight position={[50, 50, 10]} />
-      <group position={[0, 0, 0]}>
-        {/* <JackAttack scale={2} /> */}
-        <Space scale={.0015}/>
-        <ContactShadows scale={20} blur={10} far={20} />
-      </group>
-      <OrbitControls
+      <Squad />
+      {/* <JackAttack scale={2} /> */}
+      {/* <Space scale={.0015}/> */}
+      {/* <OrbitControls
         autoRotate
         maxPolarAngle={Math.PI / 3}
         minPolarAngle={Math.PI / 3}
-      />
+      /> */}
     </Canvas>
   );
 }
