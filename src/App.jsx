3b39a69e5225b3d5bd72ddd75295cd9e09af83b5
Jack Long <jack.richard.long@gmail.com>
2022-10-11 20:05:47 -0500

    voxel cartoons for links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { Suspense, useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Physics, usePlane, useBox, Debug } from "@react-three/cannon";
 import {
@@ -8,18 +8,37 @@ import {
   Html,
   useTexture,
 } from "@react-three/drei";
+import { MeshLambertMaterial } from "three";
 
-function TwitterLogo({ time, ...props }) {
-  const { scene } = useGLTF("/twitter_logo.glb");
+function TwitterBird({ time, ...props }) {
+  const { scene } = useGLTF("/voxel_twitter_bird.glb");
   const ref = useRef();
   useFrame(() => (ref.current.rotation.y += 0.02));
   return (
     <mesh {...props} onClick={(e) => alert("HI")} ref={ref}>
-      <primitive object={scene} scale={0.006} />;
+      <primitive object={scene} scale={0.05} />;
       <Html distanceFactor={20}>
         <div className="content">
           <a href="https://twitter.com/jack_attacking" target="_black">
             @JackAttack
+          </a>
+        </div>
+      </Html>
+    </mesh>
+  );
+}
+
+function GithubCat({ time, ...props }) {
+  const { scene } = useGLTF("/voxel_github_cat.glb");
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.y = ref.current.rotation.z += 0.02));
+  return (
+    <mesh {...props} onClick={(e) => alert("HI")} ref={ref}>
+      <primitive object={scene} scale={0.3} />;
+      <Html distanceFactor={20}>
+        <div className="content">
+          <a href="https://twitter.com/jack_attacking" target="_black">
+            Github
           </a>
         </div>
       </Html>
@@ -43,7 +62,6 @@ function LinkedIn({ time, ...props }) {
 
         // onPointerOver={() => setHover(true)}
         // onPointerOut={() => setHover(false)}
-        // rotateX={Math.PI / 2}
       />
       ;
       <Html distanceFactor={20} onClick={(e) => console.log("click")}>
@@ -110,21 +128,36 @@ function Dodecahedron({ time, ...props }) {
   );
 }
 
+function TwitterDodecha({ time, ...props }) {
+  const texture = useTexture("/Chroma Blue.jpg");
+
+  return (
+    <mesh {...props}>
+      <dodecahedronGeometry />
+      <meshStandardMaterial map={texture} toneMapped={false} />
+      <Html distanceFactor={10}>
+        <div className="content">
+          hello <br />
+          world
+        </div>
+      </Html>
+    </mesh>
+  );
+}
+
 function Squad() {
   const orbitRef = useRef();
   useFrame(
-    () =>
-      (orbitRef.current.rotation.x = orbitRef.current.rotation.y +=
-          0.01)
+    () => (orbitRef.current.rotation.x = orbitRef.current.rotation.y += 0.01)
   );
   return (
     <group>
       <M1 />
       <group ref={orbitRef}>
-        <LinkedIn position={[0, -7, 0]}/>
+        <GithubCat position={[0, -7, 0]}/>
         <Dodecahedron position={[0, 0, 7]} />
         <Dodecahedron position={[0, 0, -7]} />
-        <TwitterLogo position={[0, 7, 0]} />
+        <TwitterBird position={[0, 7, 0]} />
       </group>
     </group>
   );
@@ -133,16 +166,18 @@ function Squad() {
 export default function App() {
   return (
     <Canvas camera={{ position: [0, 0, 15] }}>
-      <hemisphereLight color="white" groundColor="blue" intensity={0.75} />
-      <spotLight position={[50, 50, 10]} />
-      <Squad />
-      {/* <JackAttack scale={2} /> */}
-      {/* <Space scale={.0015}/> */}
-      {/* <OrbitControls
+      <Suspense fallback={null}>
+        <hemisphereLight color="white" groundColor="blue" intensity={0.75} />
+        <spotLight position={[50, 50, 10]} />
+        <Squad />
+        {/* <JackAttack scale={2} /> */}
+        {/* <Space scale={.0015}/> */}
+        {/* <OrbitControls
         autoRotate
         maxPolarAngle={Math.PI / 3}
         minPolarAngle={Math.PI / 3}
       /> */}
+      </Suspense>
     </Canvas>
   );
 }
