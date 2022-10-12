3b79d0a3baa2070166b619a1575c37ee4def4ba8
Jack Long <jack.richard.long@gmail.com>
2022-10-12 15:35:42 -0500

    change typing message

--- src/computer/typing.jsx
+++ src/computer/typing.jsx
@@ -3,7 +3,7 @@ import TypeIt from "typeit-react";
 export const Typing = () => {
   return (
     <div>
-      <TypeIt>Hey there, I am Jack Attack.  Welcome to my site.  Please click on any of the floating objects to be redirected to my various pages.  This site was built with React Three, a fanstatic ThreeJS framwork for React written by Poimdandres</TypeIt>
+      <TypeIt element={"h1"}>Hey there, I am Jack Attack!</TypeIt>
     </div>
   );
 };
