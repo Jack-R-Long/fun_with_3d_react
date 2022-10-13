0a86a4eaf62a77a7fe460fcff5c54fcd685241a8
Jack Long <jack.richard.long@gmail.com>
2022-10-12 21:33:51 -0500

    version 1 done!  Typing complete and styled

--- src/computer/typing.jsx
+++ src/computer/typing.jsx
@@ -2,8 +2,26 @@ import TypeIt from "typeit-react";
 
 export const Typing = () => {
   return (
-    <div>
-      <TypeIt element={"h1"}>Hey there, I am Jack Attack!</TypeIt>
+    <div className="computer-text">
+      <h1>
+        <TypeIt
+          options={{
+            strings: [
+              "Hey there I am Jack Attack",
+              " ",
+              "I love building things, breaking things, and talking $%!@",
+              " ",
+              "Click the computer and drag it around for fun!",
+              " ",
+              "Click the bird to go to my Twitter ♥️",
+              " ",
+              "Click the cat to go to my Github ♥️",
+            ],
+            speed: 80,
+            waitUntilVisible: true,
+          }}
+        />
+      </h1>
     </div>
   );
 };
